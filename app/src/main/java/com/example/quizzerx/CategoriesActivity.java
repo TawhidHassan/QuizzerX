0ca0e67c042c0384ee41a51df1a27fd416242170
sifat hassan <sifat27.sh22@gmail.com>
2020-03-06 14:52:31 +0600

    retrive category data from firebase

--- app/src/main/java/com/example/quizzerx/CategoriesActivity.java
+++ app/src/main/java/com/example/quizzerx/CategoriesActivity.java
@@ -1,16 +1,16 @@
 package com.example.quizzerx;
 
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 import android.widget.Toolbar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -25,6 +25,9 @@ import java.util.List;
 public class CategoriesActivity extends AppCompatActivity {
     private androidx.appcompat.widget.Toolbar toolbarx;
     RecyclerView recyclerView;
+
+
+    List<CategoryModel>categoryModelList;
     // Write a message to the database
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRef = database.getReference();
@@ -33,49 +36,42 @@ public class CategoriesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
-        recyclerView=findViewById(R.id.categoryRecyclerViewId);
+        recyclerView = findViewById(R.id.categoryRecyclerViewId);
         toolbarx = findViewById(R.id.toolbarId);
         setSupportActionBar(toolbarx);
         getSupportActionBar().setTitle("Category");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
-        LinearLayoutManager layoutManager=new LinearLayoutManager(this);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         layoutManager.setOrientation(RecyclerView.VERTICAL);
         recyclerView.setLayoutManager(layoutManager);
 
-        List<CategoryModel>categoryModelList=new ArrayList<>();
-        categoryModelList.add(new CategoryModel("","sifat"));
-        categoryModelList.add(new CategoryModel("","sifat"));
-        categoryModelList.add(new CategoryModel("","sifat"));
-        categoryModelList.add(new CategoryModel("","sifat"));
-        categoryModelList.add(new CategoryModel("","sifat"));
-        categoryModelList.add(new CategoryModel("","sifat"));
-        categoryModelList.add(new CategoryModel("","sifat"));
-        CategoryAdapter adapter=new CategoryAdapter(categoryModelList);
+        categoryModelList=new ArrayList<>();
+
+        final CategoryAdapter adapter=new CategoryAdapter(categoryModelList);
         recyclerView.setAdapter(adapter);
 
-        myRef.child("categories").child("category1").child("name").addListenerForSingleValueEvent(new ValueEventListener() {
+        myRef.child("categories").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                Toast.makeText(CategoriesActivity.this,dataSnapshot.getValue().toString(),Toast.LENGTH_LONG).show();
+                for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren())
+                {
+                    categoryModelList.add(dataSnapshot1.getValue(CategoryModel.class));
+                    //why we use CatehoryModel.class
+                    //because .>>>amara category model a direct firebase ar cildren name gyula use korsi tai aikhana just category model call korlai hoba.
+                    //ar jodi firebase ar direct name use na kortam tahola amadar alada alada vaba model diya data nita hoto
+
+                }
+                adapter.notifyDataSetChanged();
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
-
+                Toast.makeText(CategoriesActivity.this,databaseError.getMessage(),Toast.LENGTH_LONG).show();
             }
         });
 
 
-    }
-
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            finish();
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
